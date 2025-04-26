bad47b52a3bc0484333920cf5b7411857ae02b90
vinay <thevpspro@gmail.com>
2025-04-26 10:12:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './components/Home.jsx'
+
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen' >
+      <Home></Home>
+    </div>
+  )
+}
+
+export default App 
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => {
+  document.title = 'SCSDB | Homepage'
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
